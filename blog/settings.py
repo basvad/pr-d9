949356aa565a79f9e7ad0482467f11d1ed563781
Vadim Basov <basvad@outlook.com>
2020-07-25 12:23:18 +0300

    deploy4

--- blog/settings.py
+++ blog/settings.py
@@ -76,8 +76,20 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 #для деплоя на heroku
-import dj_database_url  
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+#import dj_database_url  
+#DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+LANGUAGE_CODE = 'ru-RU'
+
+
+django_heroku.settings(locals())
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
